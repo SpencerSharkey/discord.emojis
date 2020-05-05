05d2074eb4067292b7a3d6c834d12de26a4d713a
devcat <c4t@tuta.io>
2020-05-05 13:22:57 +0200

    :flushed:

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+  kv: require('./kv.json'),
+  default: require('./emojis.json'),
+  metadata: require('./metadata.json'),
+}
---
+++ src/run.js
@@ -0,0 +1,62 @@
+const { get } = require('https');
+const { execSync } = require('child_process');
+const { readFileSync, writeFileSync } = require('fs');
+
+async function main() {
+  const emojis = readFileSync('./dist/emojis.json').toString();
+  const prev = JSON.parse(readFileSync('./dist/metadata.json').toString());
+
+  const discord_html = await fetch('https://discord.com/channels/@me');
+  const current = await find(emojis, prev.hash, discord_html.toString());
+  
+  if (!current) process.exit(0);
+
+  execSync('git config --local user.name "github-actions[bot]"');
+  execSync('git config --local user.email "github-actions[bot]@users.noreply.github.com"');
+
+  execSync('git commit -m "update emojis" -a');
+  execSync(`git push https://${process.env.ACTOR}:${process.env.GITHUB_TOKEN}@github.com/devcat/discord.emojis.git HEAD:master`);
+}
+
+main();
+
+function fetch(url) {
+  return new Promise((resolve, reject) => {
+    get(url, response => {
+      const chunks = [];
+      response.once('error', reject);
+      response.on('data', chunk => chunks.push(chunk));
+      response.once('end', () => resolve(Buffer.concat(chunks)));
+    }).end();
+  });
+}
+
+const JS_ASSET = /assets\/([\d\w]+.js)/g;
+const INLINED_EMOJIS_JSON = /JSON.parse\('({"people":[^']+})'\)}/;
+
+async function find(old, hash, html) {
+  for (const [, asset] of html.matchAll(JS_ASSET)) {
+    if (hash === asset) return null;
+    html = await fetch(`https://discord.com/assets/${asset}`);
+
+    html = html.toString();
+    const g = html.match(INLINED_EMOJIS_JSON);
+
+    if (!g) continue;
+    if (old === g[1]) return null;
+    const emojis = JSON.parse(g[1]);
+
+    const kv = {};
+    for (const cat in emojis) {
+      for (const emoji of emojis[cat]) {
+        for (const name of emoji.names) kv[name] = emoji.surrogates;
+      }
+    }
+
+    writeFileSync('./dist/kv.json', JSON.stringify(kv));
+    writeFileSync('./dist/emojis.json', JSON.stringify(emojis));
+    writeFileSync('./dist/metadata.json', JSON.stringify({ hash: asset }));
+
+    return true;
+  }
+}
